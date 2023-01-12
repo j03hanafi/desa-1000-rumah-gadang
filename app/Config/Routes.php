f8d5d2b9390188e41ed703d195fa855bfdcfe99b
j03hanafi <namoapak14@gmail.com>
2023-01-13 00:18:43 +0700

    updated permission for owner

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -78,16 +78,17 @@ $routes->group('web', ['namespace' => 'App\Controllers\Web'], function($routes) 
 });
 
 // Dashboard
-$routes->group('dashboard', ['namespace' => 'App\Controllers\Web', 'filter' => 'role:admin,owner'], function($routes) {
+$routes->group('dashboard', ['namespace' => 'App\Controllers\Web', 'filter' => 'role:admin'], function($routes) {
     $routes->get('/', 'Dashboard::index');
-    $routes->get('rumahGadang', 'Dashboard::rumahGadang',  ['filter' => 'role:owner']);
-    $routes->get('event', 'Dashboard::event',  ['filter' => 'role:owner']);
+    $routes->get('rumahGadang', 'Dashboard::rumahGadang',  ['filter' => 'role:admin']);
+    $routes->get('event', 'Dashboard::event',  ['filter' => 'role:admin']);
+    $routes->get('event', 'Dashboard::event',  ['filter' => 'role:admin']);
     $routes->get('facility', 'Dashboard::facility', ['filter' => 'role:admin']);
     $routes->get('recommendation', 'Dashboard::recommendation',  ['filter' => 'role:admin']);
     $routes->get('users', 'Dashboard::users', ['filter' => 'role:admin']);
     
-    $routes->presenter('rumahGadang',  ['filter' => 'role:owner']);
-    $routes->presenter('event',  ['filter' => 'role:owner']);
+    $routes->presenter('rumahGadang',  ['filter' => 'role:admin']);
+    $routes->presenter('event',  ['filter' => 'role:admin']);
     $routes->presenter('facility', ['filter' => 'role:admin']);
     $routes->presenter('users', ['filter' => 'role:admin']);
 });
